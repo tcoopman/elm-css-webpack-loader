44641d0b83772bec934d5437fdfa8c413a624df2
W. Brian Gourlie <bgourlie@gmail.com>
2016-09-11 23:01:31 -0500

    Fix embed call
    The API was changed for embedding modules -- This fixes the example being broken when running it.

--- examples/index.js
+++ examples/index.js
@@ -3,4 +3,4 @@
 require('./index.html');
 require('./src/Stylesheets');
 var Elm = require('./src/HomepageView');
-Elm.embed(Elm.HomepageView, document.getElementById('main'));
+Elm.HomepageView.embed(document.getElementById('main'));
